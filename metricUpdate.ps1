ec32beba832da545eca273858c5702db6f8faa9d
Markus Wessjohann <23458894+MarkusWessjohann@users.noreply.github.com>
2019-01-23 12:41:43 +0100

    insert comment

--- metricUpdate.ps1
+++ metricUpdate.ps1
@@ -1,3 +1,12 @@
+# Author: MarkusWessjohann
+# Date: 24.08.2017
+# Desc: metric keyword substitution 
+#       Inserts : 
+#       * Complexity
+#       * LineDiff
+#       * LOC
+#       * # Commits
+#       * techn. Debt
 Param(
  [String[]]$files
 )
